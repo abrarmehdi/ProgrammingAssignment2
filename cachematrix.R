1bc1d3dd6e2a4b301720c2ac4fcfffb25cf5c674
Abrar Mehdi <35421321+abrarmehdi@users.noreply.github.com>
2018-12-16 21:13:56 +0530

    Introduced two functions to calculate matrix inversion
    Calculating the matrix inversion and storing it in cache so that we should not be calculating the inversion again for the same matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,63 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation.
+## Two functions here calculates the Matrix inversion and stores it in cache so that it doesn't have to calculate the inversion again for the same matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a new type of data structure.
+## Using this data structure we can store the matrix inversion value along with the original matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        ## Initializing the matrixInverse variable to NULL
+        matrixInverse <- NULL
+        
+        ## setMatrix function
+        setMatrix <- function(y) {
+                x <<- y
+                matrixInverse <<- NULL
+        }
+        
+        ## getMatrix function - returns the matrix
+        getMatrix <- function() x
+        
+        ## setInverse function - sets the matrix inversion value
+        setInverse <- function(mi) matrixInverse <<- mi
+        
+        ## getInverse function - returns the matrix inversion value
+        getInverse <- function() matrixInverse
+        
+        ## list to store all the values and functions
+        list(x = x, getInverse = getInverse, setInverse = setInverse, getMatrix = getMatrix, setMatrix = setMatrix)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks the new data structure for matrix inversion
+## If it finds the matrix inversion then it will directly return the value without calculating
+## If the value of matrix inversion is unavailable then it will calculate the matrix inversion value and stores it in the data structure
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+        ## Retrieving the value of inverse
+        matrixInverse <- x$getInverse()
+        
+        ## Checking if the value is NULL
+        if(!is.null(matrixInverse)) {
+                
+                ## If the value is not null then displaying the message
+                message("Inverse Already Calculated. Retrieving from Cache...")
+                
+                ## Returning the value of already calculated matrix inverse
+                return(matrixInverse)
+        }
+        
+        ## Retrieving the matrix
+        data <- x$getMatrix()
+        
+        ## Since all the matrices in this example are invertible we are not checking for square invertiblness of a matrix
+        ## Using the solve function we are calculating the matrix inversion
+        matrixInverse <- solve(data)
+        
+        ## Upon calculation of matrix inversion, storing the value using setInverse function
+        x$setInverse(matrixInverse)
+        
+        ## Returning the value of matrix inversion
+        matrixInverse
 }
